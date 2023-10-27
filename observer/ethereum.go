030d82f0ad223652c977a7f0fbb67400ea4ec518
Cedric Fung <cedric@vec.io>
2023-10-27 14:10:00 +0000

    fix ethereum name typo

---
+++ observer/ethereum.go
@@ -0,0 +1,14 @@
+package observer
+
+import "github.com/MixinNetwork/safe/keeper"
+
+func (node *Node) ethereumParams(chain byte) (string, string) {
+	switch chain {
+	case keeper.SafeChainEthereum:
+		return node.conf.EthereumRPC, keeper.SafeEthereumChainId
+	case keeper.SafeChainMVM:
+		return node.conf.MVMRPC, keeper.SafeMVMChainId
+	default:
+		panic(chain)
+	}
+}
