534ed232c6083c496b000c1b0e6de42bd6ab7123
hundredark <hundredark@gmail.com>
2024-01-08 17:43:32 +0800

    Fix recovery state updating

--- observer/store.go
+++ observer/store.go
@@ -661,8 +661,8 @@ func (s *SQLite3Store) UpdateRecoveryState(ctx context.Context, address, raw str
 		err = s.execOne(ctx, tx, "UPDATE recoveries SET state=?, raw_transaction=?, updated_at=? WHERE address=? AND state=?",
 			state, raw, time.Now().UTC(), address, common.RequestStateInitial)
 	case common.RequestStateDone:
-		err = s.execOne(ctx, tx, "UPDATE recoveries SET state=?, updated_at=? WHERE address=? AND state=?",
-			state, time.Now().UTC(), address, common.RequestStatePending)
+		err = s.execOne(ctx, tx, "UPDATE recoveries SET state=?, raw_transaction=?, updated_at=? WHERE address=? AND state IN (?, ?)",
+			state, raw, time.Now().UTC(), address, common.RequestStateInitial, common.RequestStatePending)
 	default:
 		panic(state)
 	}
