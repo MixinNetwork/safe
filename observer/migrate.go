5fda60c25222270455f26ab220f36a1510ffa6c3
hundredark <hundredark@gmail.com>
2024-01-11 11:53:40 +0800

    Add observer migrate

--- observer/migrate.go
+++ observer/migrate.go
@@ -1,4 +1,4 @@
-package signer
+package observer
 
 import (
 	"context"
@@ -17,36 +17,24 @@ func (s *SQLite3Store) migrate(ctx context.Context) error {
 	}
 	defer tx.Rollback()
 
-	existed, err := s.checkExistence(ctx, tx, "SELECT session_id FROM sessions LIMIT 1")
-	if err != nil || !existed {
-		return err
-	}
-
-	key, val := "SCHEMA:VERSION:20230830", ""
+	key, val := "SCHEMA:VERSION:664f750de4e357782227616ac6c6b5b050df2bbf", ""
 	row := tx.QueryRowContext(ctx, "SELECT value FROM properties WHERE key=?", key)
 	err = row.Scan(&val)
 	if err == nil || err != sql.ErrNoRows {
 		return err
 	}
 
-	query := "ALTER TABLE sessions ADD COLUMN committed_at TIMESTAMP;\n"
-	query = query + "ALTER TABLE sessions ADD COLUMN prepared_at TIMESTAMP;\n"
-	query = query + "ALTER TABLE session_signers ADD COLUMN updated_at TIMESTAMP;"
+	query := "ALTER TABLE deposits ADD COLUMN asset_address VARCHAR;"
 	_, err = tx.ExecContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	_, err = tx.ExecContext(ctx, "UPDATE deposits SET asset_address=?", "")
 	if err != nil {
 		return err
 	}
 
 	now := time.Now().UTC()
-	_, err = tx.ExecContext(ctx, "UPDATE sessions SET committed_at=?, prepared_at=?", now, now)
-	if err != nil {
-		return err
-	}
-	_, err = tx.ExecContext(ctx, "UPDATE session_signers SET updated_at=?", now)
-	if err != nil {
-		return err
-	}
-
 	_, err = tx.ExecContext(ctx, "INSERT INTO properties (key, value, created_at) VALUES (?, ?, ?)", key, query, now)
 	if err != nil {
 		return err
