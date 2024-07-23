b07fd2dc4003f9552fab9024165632fdb3eb0e2c
Hundredark <hundredark@gmail.com>
2024-07-23 16:27:35 +0800

    No updated_at in signer properties (#79)

--- signer/store.go
+++ signer/store.go
@@ -65,7 +65,7 @@ func (s *SQLite3Store) Migrate(ctx context.Context) error {
 	}
 
 	now := time.Now().UTC()
-	_, err = tx.ExecContext(ctx, "INSERT INTO properties (key, value, created_at, updated_at) VALUES (?, ?, ?, ?)", key, query, now, now)
+	_, err = tx.ExecContext(ctx, "INSERT INTO properties (key, value, created_at) VALUES (?, ?, ?)", key, query, now)
 	if err != nil {
 		return err
 	}
