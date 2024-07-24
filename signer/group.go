bc5df1327adb30cd2d346b523953eb4d09156e59
hundredark <hundredark@gmail.com>
2024-07-24 14:10:56 +0800

    Back up key before save

--- signer/group.go
+++ signer/group.go
@@ -488,6 +488,13 @@ func (node *Node) startKeygen(ctx context.Context, op *common.Operation) error {
 		return node.store.FailSession(ctx, op.Id)
 	}
 	op.Public = hex.EncodeToString(res.Public)
+	err = node.sendKeygenBackup(ctx, op, res.Share)
+	logger.Printf("node.sendKeygenBackup(%v, %d) => %v", op, len(res.Share), err)
+	if err != nil {
+		err = node.store.FailSession(ctx, op.Id)
+		logger.Printf("store.FailSession(%s, startKeygen) => %v", op.Id, err)
+		return err
+	}
 	return node.store.WriteKeyIfNotExists(ctx, op.Id, op.Curve, op.Public, res.Share)
 }
 
