fee6c885737851b8839d89f0aad01453c2298799
hundredark <hundredark@gmail.com>
2024-10-23 20:01:04 +0800

    fix too many logs

--- keeper/store/export.go
+++ keeper/store/export.go
@@ -528,7 +528,7 @@ func (s *SQLite3Store) listProperties(ctx context.Context) ([]*Property, error) 
 func (s *SQLite3Store) exportRequests(ctx context.Context, tx *sql.Tx, requests []*common.Request) error {
 	for _, req := range requests {
 		vals := []any{req.Id, req.MixinHash.String(), req.MixinIndex, req.AssetId, req.Amount, req.Role, req.Action, req.Curve, req.Holder, req.ExtraHEX, req.State, req.CreatedAt, req.UpdatedAt, req.Sequence}
-		err := s.execOne(ctx, tx, buildInsertionSQL("requests", requestCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("requests", requestCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT requests %v", err)
 		}
@@ -539,7 +539,7 @@ func (s *SQLite3Store) exportRequests(ctx context.Context, tx *sql.Tx, requests 
 func (s *SQLite3Store) exportNetworkInfos(ctx context.Context, tx *sql.Tx, infos []*NetworkInfo) error {
 	for _, info := range infos {
 		vals := []any{info.RequestId, info.Chain, info.Fee, info.Height, info.Hash, info.CreatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("network_infos", infoCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("network_infos", infoCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT network_infos %v", err)
 		}
@@ -552,7 +552,7 @@ func (s *SQLite3Store) exportOperationParams(ctx context.Context, tx *sql.Tx, op
 		amount := params.OperationPriceAmount.String()
 		minimum := params.TransactionMinimum.String()
 		vals := []any{params.RequestId, params.Chain, params.OperationPriceAsset, amount, minimum, params.CreatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("operation_params", paramsCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("operation_params", paramsCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT operation_params %v", err)
 		}
@@ -563,7 +563,7 @@ func (s *SQLite3Store) exportOperationParams(ctx context.Context, tx *sql.Tx, op
 func (s *SQLite3Store) exportAssets(ctx context.Context, tx *sql.Tx, assets []*Asset) error {
 	for _, asset := range assets {
 		vals := []any{asset.AssetId, asset.MixinId, asset.AssetKey, asset.Symbol, asset.Name, asset.Decimals, asset.Chain, asset.CreatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("assets", assetCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("assets", assetCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT assets %v", err)
 		}
@@ -574,7 +574,7 @@ func (s *SQLite3Store) exportAssets(ctx context.Context, tx *sql.Tx, assets []*A
 func (s *SQLite3Store) exportKeys(ctx context.Context, tx *sql.Tx, keys []*Key) error {
 	for _, key := range keys {
 		vals := []any{key.Public, key.Curve, key.RequestId, key.Role, key.Extra, key.Flags, key.Holder, key.CreatedAt, key.UpdatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("keys", keyCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("keys", keyCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT keys %v", err)
 		}
@@ -584,7 +584,7 @@ func (s *SQLite3Store) exportKeys(ctx context.Context, tx *sql.Tx, keys []*Key) 
 
 func (s *SQLite3Store) exportSafeProposals(ctx context.Context, tx *sql.Tx, proposals []*SafeProposal) error {
 	for _, sp := range proposals {
-		err := s.execOne(ctx, tx, buildInsertionSQL("safe_proposals", safeProposalCols), sp.values()...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("safe_proposals", safeProposalCols), sp.values()...)
 		if err != nil {
 			return fmt.Errorf("INSERT safe_proposals %v", err)
 		}
@@ -594,7 +594,7 @@ func (s *SQLite3Store) exportSafeProposals(ctx context.Context, tx *sql.Tx, prop
 
 func (s *SQLite3Store) exportSafes(ctx context.Context, tx *sql.Tx, safes []*Safe) error {
 	for _, safe := range safes {
-		err := s.execOne(ctx, tx, buildInsertionSQL("safes", safeCols), safe.values()...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("safes", safeCols), safe.values()...)
 		if err != nil {
 			return fmt.Errorf("INSERT safes %v", err)
 		}
@@ -607,7 +607,7 @@ func (s *SQLite3Store) exportBitcoinOutputs(ctx context.Context, tx *sql.Tx, out
 		script := hex.EncodeToString(utxo.Script)
 		cols := []string{"transaction_hash", "output_index", "address", "satoshi", "script", "sequence", "chain", "state", "spent_by", "request_id", "created_at", "updated_at"}
 		vals := []any{utxo.TransactionHash, utxo.Index, utxo.Address, utxo.Satoshi, script, utxo.Sequence, utxo.Chain, utxo.State, utxo.SpentBy, utxo.RequestId, utxo.CreatedAt, utxo.CreatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("bitcoin_outputs", cols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("bitcoin_outputs", cols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT bitcoin_outputs %v", err)
 		}
@@ -619,7 +619,7 @@ func (s *SQLite3Store) exportEthereumBalances(ctx context.Context, tx *sql.Tx, b
 	for _, balance := range balances {
 		cols := []string{"address", "asset_id", "asset_address", "safe_asset_id", "balance", "latest_tx_hash", "updated_at"}
 		vals := []any{balance.Address, balance.AssetId, balance.AssetAddress, balance.SafeAssetId, balance.balance, balance.LatestTxHash, balance.UpdatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("ethereum_balances", cols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("ethereum_balances", cols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT ethereum_balances %v", err)
 		}
@@ -630,7 +630,7 @@ func (s *SQLite3Store) exportEthereumBalances(ctx context.Context, tx *sql.Tx, b
 func (s *SQLite3Store) exportDeposits(ctx context.Context, tx *sql.Tx, deposits []*Deposit) error {
 	for _, deposit := range deposits {
 		vals := []any{deposit.TransactionHash, deposit.OutputIndex, deposit.AssetId, deposit.Amount, deposit.Receiver, deposit.Sender, deposit.State, deposit.Chain, deposit.Holder, deposit.Category, deposit.CreatedAt, deposit.CreatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("deposits", depositsCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("deposits", depositsCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT deposits %v", err)
 		}
@@ -641,7 +641,7 @@ func (s *SQLite3Store) exportDeposits(ctx context.Context, tx *sql.Tx, deposits 
 func (s *SQLite3Store) exportTransactions(ctx context.Context, tx *sql.Tx, transactions []*Transaction) error {
 	for _, trx := range transactions {
 		vals := []any{trx.TransactionHash, trx.RawTransaction, trx.Holder, trx.Chain, trx.AssetId, trx.State, trx.Data, trx.RequestId, trx.CreatedAt, trx.UpdatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("transactions", transactionCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("transactions", transactionCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT transactions %v", err)
 		}
@@ -652,7 +652,7 @@ func (s *SQLite3Store) exportTransactions(ctx context.Context, tx *sql.Tx, trans
 func (s *SQLite3Store) exportSignatureRequests(ctx context.Context, tx *sql.Tx, signatures []*SignatureRequest) error {
 	for _, r := range signatures {
 		vals := []any{r.RequestId, r.TransactionHash, r.InputIndex, r.Signer, r.Curve, r.Message, r.Signature, r.State, r.CreatedAt, r.UpdatedAt}
-		err := s.execOne(ctx, tx, buildInsertionSQL("signature_requests", signatureCols), vals...)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("signature_requests", signatureCols), vals...)
 		if err != nil {
 			return fmt.Errorf("INSERT signature_requests %v", err)
 		}
@@ -663,7 +663,7 @@ func (s *SQLite3Store) exportSignatureRequests(ctx context.Context, tx *sql.Tx, 
 func (s *SQLite3Store) exportProperties(ctx context.Context, tx *sql.Tx, properties []*Property) error {
 	for _, p := range properties {
 		cols := []string{"key", "value", "created_at"}
-		err := s.execOne(ctx, tx, buildInsertionSQL("properties", cols), p.Key, p.Value, p.CreatedAt)
+		_, err := tx.ExecContext(ctx, buildInsertionSQL("properties", cols), p.Key, p.Value, p.CreatedAt)
 		if err != nil {
 			return fmt.Errorf("INSERT properties %v", err)
 		}
