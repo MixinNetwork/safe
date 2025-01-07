2a8a2d7e70a3adb3b577db2371d94b0db77bcb64
hundredark <hundredark@gmail.com>
2025-01-07 15:55:43 +0800

    typo

--- computer/store/withdrawal.go
+++ computer/store/withdrawal.go
@@ -4,15 +4,31 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+
+	"github.com/MixinNetwork/safe/common"
 )
 
 var confirmedWithdrawalCols = []string{"hash", "trace_id", "call_id", "created_at"}
 
 func (s *SQLite3Store) writeConfirmedWithdrawal(ctx context.Context, tx *sql.Tx, req *Request, hash, traceId, callId string) error {
 	vals := []any{hash, traceId, callId, req.CreatedAt}
-	err := s.execOne(ctx, tx, buildInsertionSQL("confirmed_withdrawals", systemCallCols), vals...)
+	err := s.execOne(ctx, tx, buildInsertionSQL("confirmed_withdrawals", confirmedWithdrawalCols), vals...)
 	if err != nil {
 		return fmt.Errorf("INSERT confirmed_withdrawals %v", err)
 	}
 	return nil
+}
+
+func (s *SQLite3Store) IsConfirmedWithdrawal(ctx context.Context, hash string) (bool, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return false, err
+	}
+	defer common.Rollback(tx)
+
+	existed, err := s.checkExistence(ctx, tx, "SELECT trace_id FROM confirmed_withdrawals WHERE hash=?", hash)
+	return existed, err
 }
