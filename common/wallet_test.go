d337e2fa4a13babe023e514fa48f178f4b75bfb8
hundredark <hundredark@gmail.com>
2025-06-19 12:29:51 +0800

    add ListUnspentUTXOsByAsset

--- common/wallet_test.go
+++ common/wallet_test.go
@@ -51,14 +51,31 @@ func TestWallet(t *testing.T) {
 	}
 	err = mw.writeOutputsIfNotExists(ctx, os)
 	require.Nil(err)
+	outputs, total, err := mw.store.ListUnspentUTXOsByAsset(ctx, mtg.StorageAssetId)
+	require.Nil(err)
+	require.Len(outputs, 8)
+	require.Equal("1.2", total.String())
+	outputs, total, err = mw.store.ListUnspentUTXOsByAsset(ctx, SafeSolanaChainId)
+	require.Nil(err)
+	require.Len(outputs, 4)
+	require.Equal("0.4", total.String())
 
 	id := uuid.Must(uuid.NewV4()).String()
 	os1, err := mw.LockUTXOs(ctx, id, mtg.StorageAssetId, decimal.RequireFromString("0.5"))
 	require.Nil(err)
 	require.Len(os1, 3)
+	outputs, total, err = mw.store.ListUnspentUTXOsByAsset(ctx, mtg.StorageAssetId)
+	require.Nil(err)
+	require.Len(outputs, 5)
+	require.Equal("0.6", total.String())
+
 	os2, err := mw.LockUTXOs(ctx, id, mtg.StorageAssetId, decimal.RequireFromString("0.5"))
 	require.Nil(err)
 	require.Len(os2, 3)
+	outputs, total, err = mw.store.ListUnspentUTXOsByAsset(ctx, mtg.StorageAssetId)
+	require.Nil(err)
+	require.Len(outputs, 5)
+	require.Equal("0.6", total.String())
 	for i, o1 := range os1 {
 		o2 := os2[i]
 		require.Equal(o1.OutputID, o2.OutputID)
@@ -73,12 +90,20 @@ func TestWallet(t *testing.T) {
 	os, err = mw.LockUTXOs(ctx, id, mtg.StorageAssetId, decimal.RequireFromString("0.5"))
 	require.Nil(err)
 	require.Len(os, 4)
+	outputs, total, err = mw.store.ListUnspentUTXOsByAsset(ctx, mtg.StorageAssetId)
+	require.Nil(err)
+	require.Len(outputs, 1)
+	require.Equal("0.1", total.String())
 
 	id = uuid.Must(uuid.NewV4()).String()
 	os, err = mw.LockUTXOs(ctx, id, mtg.StorageAssetId, decimal.RequireFromString("0.2"))
 	require.NotNil(err)
 	require.True(strings.Contains(err.Error(), "insufficient outputs"))
 	require.Len(os, 0)
+	outputs, total, err = mw.store.ListUnspentUTXOsByAsset(ctx, mtg.StorageAssetId)
+	require.Nil(err)
+	require.Len(outputs, 1)
+	require.Equal("0.1", total.String())
 }
 
 func buildOutput(assetId, amount string, sequence uint64) *mixin.SafeUtxo {
