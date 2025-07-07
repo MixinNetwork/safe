a62f905b3df128e74e3fdb2a0f8d8b0c5eb8c24a
hundredark <hundredark@gmail.com>
2025-07-07 17:44:53 +0800

    fix WriteCache

--- observer/store.go
+++ observer/store.go
@@ -1009,7 +1009,7 @@ func (s *SQLite3Store) ReadCache(ctx context.Context, k string, d time.Duration)
 	return value, nil
 }
 
-func (s *SQLite3Store) WriteCache(ctx context.Context, k, v string, duration time.Duration) error {
+func (s *SQLite3Store) WriteCache(ctx context.Context, k, v string) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
@@ -1019,7 +1019,7 @@ func (s *SQLite3Store) WriteCache(ctx context.Context, k, v string, duration tim
 	}
 	defer common.Rollback(tx)
 
-	threshold := time.Now().Add(-duration).UTC()
+	threshold := time.Now().Add(-cacheTTL).UTC()
 	_, err = tx.ExecContext(ctx, "DELETE FROM caches WHERE created_at<?", threshold)
 	if err != nil {
 		return err
