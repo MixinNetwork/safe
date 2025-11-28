148223e301686fecd2a50947a68096a72e8f6f8e
Cedric Fung <cedric@vec.io>
2025-11-28 13:55:31 +0000

    remove duplications

--- keeper/store/inheritance.go
+++ keeper/store/inheritance.go
@@ -47,41 +47,26 @@ func (s *SQLite3Store) processInheritanceLockOperation(ctx context.Context, tx *
 }
 
 func (s *SQLite3Store) ReadInheritanceLock(ctx context.Context, id string) (*InheritanceLock, error) {
-	tx, err := s.db.BeginTx(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-	defer common.Rollback(tx)
-
-	return s.readInheritanceLock(ctx, tx, id)
+	query := fmt.Sprintf("SELECT %s FROM inheritance_locks WHERE lock_id=?", strings.Join(inheritanceLockCols, ","))
+	return s.readInheritanceLockByQuery(ctx, query, id)
 }
 
 func (s *SQLite3Store) ReadInheritanceLockByRequestId(ctx context.Context, id string) (*InheritanceLock, error) {
+	query := fmt.Sprintf("SELECT %s FROM inheritance_locks WHERE request_id=?", strings.Join(inheritanceLockCols, ","))
+	return s.readInheritanceLockByQuery(ctx, query, id)
+}
+
+func (s *SQLite3Store) readInheritanceLockByQuery(ctx context.Context, query string, params ...any) (*InheritanceLock, error) {
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
 	defer common.Rollback(tx)
 
-	query := fmt.Sprintf("SELECT %s FROM inheritance_locks WHERE request_id=?", strings.Join(inheritanceLockCols, ","))
-	row := tx.QueryRowContext(ctx, query, id)
+	row := tx.QueryRowContext(ctx, query, params...)
 
 	var lock InheritanceLock
 	err = row.Scan(&lock.LockId, &lock.RequestId, &lock.Hash, &lock.Holder, &lock.Address, &lock.Chain, &lock.Duration, &lock.State, &lock.CreatedAt, &lock.UpdatedAt)
-	if err == sql.ErrNoRows {
-		return nil, nil
-	} else if err != nil {
-		return nil, err
-	}
-	return &lock, nil
-}
-
-func (s *SQLite3Store) readInheritanceLock(ctx context.Context, tx *sql.Tx, id string) (*InheritanceLock, error) {
-	query := fmt.Sprintf("SELECT %s FROM inheritance_locks WHERE lock_id=?", strings.Join(inheritanceLockCols, ","))
-	row := tx.QueryRowContext(ctx, query, id)
-
-	var lock InheritanceLock
-	err := row.Scan(&lock.LockId, &lock.RequestId, &lock.Hash, &lock.Holder, &lock.Address, &lock.Chain, &lock.Duration, &lock.State, &lock.CreatedAt, &lock.UpdatedAt)
 	if err == sql.ErrNoRows {
 		return nil, nil
 	} else if err != nil {
@@ -102,7 +87,7 @@ func (s *SQLite3Store) ListUnfailedInheritanceLocksByHolder(ctx context.Context,
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+	defer common.CloseOrPanic(rows)
 
 	var ls []*InheritanceLock
 	for rows.Next() {
