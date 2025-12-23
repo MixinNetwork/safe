050e5bf73717d93c2027eb4ca9bbdcf764b332bc
Cedric Fung <cedric@vec.io>
2025-12-23 08:14:57 +0000

    update some migration sql

--- keeper/store/migrate.go
+++ keeper/store/migrate.go
@@ -26,20 +26,20 @@ func (s *SQLite3Store) Migrate(ctx context.Context) error {
 	}
 	now := time.Now().UTC()
 
-	query1 := "UPDATE transactions SET state=?, WHERE transaction_hash=? AND state=?;\n"
+	query1 := "UPDATE transactions SET state=? WHERE transaction_hash=? AND state=?;"
 	err = s.execOne(ctx, tx, query1, common.RequestStateDone, "6a5ccb71871db47550acd5429764d724c0e26e9ef63e214954edce2ee80a7e90", common.RequestStateFailed)
 	if err != nil {
 		return err
 	}
 
-	query2 := "UPDATE safes SET nonce=?, WHERE holder=? AND nonce=? AND state=?;\n"
+	query2 := "UPDATE safes SET nonce=? WHERE holder=? AND nonce=? AND state=?;"
 	err = s.execOne(ctx, tx, query2, 181, "02cf89b2507b8b3ceac013f675da6cf68f1ac7a3c74de2621d9d4790d67bde43a5", 180, common.RequestStateDone)
 	if err != nil {
 		return err
 	}
 
-	query3 := "UPDATE ethereum_balances SET balance=? WHERE address=? AND asset_id=? AND safe_asset_id=?;\n"
-	err = s.execOne(ctx, tx, query3, "74500000000000000000000", "0xA82bCC5b1942c3BF74C8eef18E1a6E0d8FAFA4cb", "c94ac88f-4671-3976-b60a-09064f1811e8", "b5a91ff6-a78b-3838-9fa5-225636c093d0")
+	query3 := "UPDATE ethereum_balances SET balance=? WHERE address=? AND asset_id=? AND safe_asset_id=? AND balance=?;"
+	err = s.execOne(ctx, tx, query3, "74500000000000000000000", "0xA82bCC5b1942c3BF74C8eef18E1a6E0d8FAFA4cb", "c94ac88f-4671-3976-b60a-09064f1811e8", "b5a91ff6-a78b-3838-9fa5-225636c093d0", "77000000000000000000000")
 	if err != nil {
 		return err
 	}
